103be16acbb412c284591a6147f3db160bc298c1
kiran shrestha <kieran.shrestha@gmail.com>
2019-06-29 17:28:40 +0900

    code updated with more than 4mins log

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "patch.h"
 #include "timer.h"
 
-#define INTERVAL 60
+#define INTERVAL 150   //this code has double effect 240 means 8 minutes
 #define MAXDATA 60
 #define DATAWIDTH 3
 
@@ -11,6 +11,10 @@
 /////////this code is for the thermistor change the pin with comment below/////////////////
 /////////this code is merged to master after finishing////////////////////////////////////
 //////////sends out three analog digits /////////////////////////////////////////////////
+/* This is the final code which sends out the interval before the nDEF start
+ * fixed to 5 mins and 60 max data
+ * can be used with both commercial and prited therm
+ */
 
 #include <rf430frl152h.h>
 
@@ -19,8 +23,7 @@ unsigned char ndefcount = 0;
 
 void DeviceInit(void);
 
-unsigned char secondCTR = 0;
-volatile int ADC_Volts = 0;
+unsigned char secondCTR = 0;    //now this counts 2 secs
 
 extern u08_t NFC_NDEF_Message[];
 
@@ -32,8 +35,9 @@ void main(){
 
     initISO15693(CLEAR_BLOCK_LOCKS);            // clear all block locks
     DeviceInit();
+    initTimer();\
 
-    initTimer();
+    NFC_NDEF_Message[NDEFSTART-1] = INTERVAL;
 
     while(1)    {
         __bis_SR_register(LPM3_bits + GIE);
@@ -43,9 +47,7 @@ void main(){
 void DeviceInit(void){
     P1SEL0 = 0xF0; //keep JTAG
     P1SEL1 = 0xF0; //keep JTAG
-    //  P1SEL0 = 0x00; //no JTAG
-    //  P1SEL1 = 0x00; //no JTAG
-    /*Internal HFCLK = 4MHz      LFCLK = 256KHz*/
+
     CCSCTL0 = CCSKEY;                               // Unlock CCS
     CCSCTL1 = 0;                                    // 0b = CLKIN or X-OSC is directly used for clock generation
                                                     // 1b = CLKIN or X-OSC is divided by two for clock generation
@@ -56,6 +58,8 @@ void DeviceInit(void){
     CCSCTL8 = ACLKREQEN + MCLKREQEN + SMCLKREQEN;   //disable clocks if they are not being used
     CCSCTL0_H |= 0xFF;                              // Lock CCS
 
+//    RFPMMCTL0_H = 0xA5;
+//    RFPMMCTL0 = RFPMM_EN_BATSWITCH;
 
     SD14CTL0 = SD14SSEL__ACLK + SD14DIV__64;         //Set the clk and divider for SD ADC
     SD14CTL0 = SD14EN + SD14IE + SD14SGL;
@@ -68,8 +72,8 @@ void DeviceInit(void){
 
     //  SD14CTL1 = SD14UNI + SD14INCH_2 +  SD14RBEN1 + SD14GAIN__1 + SD14FILT__AVG + SD14RATE__AVG16384; // thermistor   pin 18/ADC2/TEMP2 //SDIGAIN_1
     //       SD14CTL1 = SD14UNI + SD14INCH_2 +  SD14RBEN1 + SD14GAIN__8 + SD14FILT__AVG + SD14RATE__AVG16384; // thermistor   pin 18/ADC2/TEMP2 //SDIGAIN_8
-    SD14CTL1 = SD14UNI + SD14INCH_3 +  SD14RBEN0 + SD14GAIN__8 + SD14FILT__AVG + SD14RATE__AVG16384; // thermistor   pin 17/ADC1/TEMP1 //SDIGAIN_8
-    //      SD14CTL1 = SD14UNI + SD14INCH_3 +  SD14RBEN0 + SD14GAIN__1 + SD14FILT__AVG + SD14RATE__AVG16384; // thermistor   pin 17/ADC1/TEMP1 //SDIGAIN_1
+    //SD14CTL1 = SD14UNI + SD14INCH_3 +  SD14RBEN0 + SD14GAIN__8 + SD14FILT__AVG + SD14RATE__AVG16384; // thermistor   pin 17/ADC1/TEMP1 //SDIGAIN_8
+     SD14CTL1 = SD14UNI + SD14INCH_3 +  SD14RBEN0 + SD14GAIN__1 + SD14FILT__AVG + SD14RATE__AVG16384; // thermistor   pin 17/ADC1/TEMP1 //SDIGAIN_1
     //    SD14CTL1 = SD14UNI + SD14INCH_2 +  SD14RBEN1 + SD14GAIN__1 + SD14FILT__CIC + SD14RATE__CIC128; // thermistor   pin 18
 
 }
@@ -89,9 +93,6 @@ __interrupt void SD_ADC_ISR(void)
         volatile unsigned int i = 0;
 
         ADC_Value =  SD14MEM0; //Read the ADC Data      //sending the raw data to phone
-//        ADC_Volts = ((ADC_Value >> 7) * 900)/(16383 >> 8);
-        ADC_Volts = (ADC_Value* 900)/16384;
-
         SD14CTL0 &= ~SD14IFG;  //clear the data available interrupt
 
         ndefcount++;
@@ -130,7 +131,7 @@ __interrupt void TimerA0_ISR(void)
 {
     TA0CTL &= ~TAIFG;
     secondCTR++;
-    NFC_NDEF_Message[NDEFSTART-1] = secondCTR;
+//    NFC_NDEF_Message[NDEFSTART-1] = secondCTR;
     if(secondCTR == INTERVAL){
         secondCTR = 0;
         SD14CTL0 |= SD14SC;
--- timer.c
+++ timer.c
@@ -11,6 +11,7 @@ void initTimer(){
 	TA0CTL |= TASSEL_1 + TAIE;	//aux clock and interrup enabled
 	TA0CTL &= ~TAIFG;			//clear interrupt
 	TA0CTL |= MC_1;				//count up to ta0ccr0
+	TA0EX0 = TAIDEX_1;         // now couting 2 secs
 	TA0CCR0 = 0xFA00;
 	TA0CCTL0 |= CCIE;
 }
